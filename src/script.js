06163f9b4d3d88c203ffa818e2f2236cb3098289
korobochko-coder <korobochko.a@gmail.com>
2022-11-03 23:41:48 -0400

    weekdays

--- src/script.js
+++ src/script.js
@@ -22,6 +22,22 @@ function showTemperature(response) {
 let longitude = null;
 let latitude = null;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
 function showForecast(response) {
   let forecast = response.data.daily;
   console.log(forecast);
